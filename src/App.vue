a5da47391723787c18d1f766e6f0629606aaff1c
Donghun Jang <bhgf123@naver.com>
2021-08-24 20:19:29 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+
+    <v-app-bar app>
+      <AppHeader></AppHeader>
+    </v-app-bar>
+    <v-main>
+      <router-view/>
+    </v-main>
+    
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import AppHeader from '@/components/common/AppHeader'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+    AppHeader
+  },
+  data() {
+    return {
+      photos: []
+    }
+  },
+  mounted() {
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  }
+  
 }
-</style>
+  
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbvue/lib/mdbvue.css'
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./router/router";
+import vuetify from './plugins/vuetify'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.get["Content-Type"] = "application/json";
 
 Vue.config.productionTip = false
 
+Vue.prototype.$axios = axios;
+
 new Vue({
-  render: h => h(App),
+  router,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
